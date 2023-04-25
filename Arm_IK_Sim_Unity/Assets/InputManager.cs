f9982224b2f2e9db987ddd8bbb44f125210c7382
Cameron <55326068+Supernova1114@users.noreply.github.com>
2023-04-25 12:15:27 -0700

    Removed hungarian notation from member vars

--- Arm_IK_Sim_Unity/Assets/InputManager.cs
+++ Arm_IK_Sim_Unity/Assets/InputManager.cs
@@ -5,56 +5,56 @@ public class InputManager : MonoBehaviour
 {
     private static InputManager instance;
 
-    private PlayerInput m_playerInput;
+    private PlayerInput playerInput;
 
-    [HideInInspector] public InputAction m_armPlanarMovementAction;
-    [HideInInspector] public InputAction m_safetyAction;
+    [HideInInspector] public InputAction armPlanarMovementAction;
+    [HideInInspector] public InputAction safetyAction;
 
-    [HideInInspector] public InputAction m_armForwardAction;
-    [HideInInspector] public InputAction m_armBackwardAction;
+    [HideInInspector] public InputAction armForwardAction;
+    [HideInInspector] public InputAction armBackwardAction;
 
-    [HideInInspector] public InputAction m_speedTrimUpAction;
-    [HideInInspector] public InputAction m_speedTrimDownAction;
+    [HideInInspector] public InputAction speedTrimUpAction;
+    [HideInInspector] public InputAction speedTrimDownAction;
 
-    [HideInInspector] public InputAction m_gripPitchAction;
-    [HideInInspector] public InputAction m_gripRollAction;
+    [HideInInspector] public InputAction gripPitchAction;
+    [HideInInspector] public InputAction gripRollAction;
 
-    [HideInInspector] public InputAction m_gripOpenAction;
-    [HideInInspector] public InputAction m_gripCloseAction;
+    [HideInInspector] public InputAction gripOpenAction;
+    [HideInInspector] public InputAction gripCloseAction;
 
-    [HideInInspector] public InputAction m_pokerOutAction;
-    [HideInInspector] public InputAction m_pokerInAction;
+    [HideInInspector] public InputAction pokerOutAction;
+    [HideInInspector] public InputAction pokerInAction;
 
     private void Awake()
     {   
         instance = this;
 
-        m_playerInput = GetComponent<PlayerInput>();
+        playerInput = GetComponent<PlayerInput>();
 
         // Instantiate input actions ---------------------------------------------------------
 
-        m_armPlanarMovementAction =  m_playerInput.actions.FindAction("ArmPlanarMovement");
-        m_safetyAction = m_playerInput.actions.FindAction("Safety");
+        armPlanarMovementAction =  playerInput.actions.FindAction("ArmPlanarMovement");
+        safetyAction = playerInput.actions.FindAction("Safety");
 
-        m_armForwardAction = m_playerInput.actions.FindAction("ArmForward");
-        m_armBackwardAction = m_playerInput.actions.FindAction("ArmBackward");
+        armForwardAction = playerInput.actions.FindAction("ArmForward");
+        armBackwardAction = playerInput.actions.FindAction("ArmBackward");
 
-        m_speedTrimUpAction = m_playerInput.actions.FindAction("SpeedTrimUp");
-        m_speedTrimDownAction = m_playerInput.actions.FindAction("SpeedTrimDown");
+        speedTrimUpAction = playerInput.actions.FindAction("SpeedTrimUp");
+        speedTrimDownAction = playerInput.actions.FindAction("SpeedTrimDown");
 
-        m_gripPitchAction = m_playerInput.actions.FindAction("GripPitch");
-        m_gripRollAction = m_playerInput.actions.FindAction("GripRoll");
+        gripPitchAction = playerInput.actions.FindAction("GripPitch");
+        gripRollAction = playerInput.actions.FindAction("GripRoll");
 
-        m_gripOpenAction = m_playerInput.actions.FindAction("GripOpen");
-        m_gripCloseAction = m_playerInput.actions.FindAction("GripClose");
+        gripOpenAction = playerInput.actions.FindAction("GripOpen");
+        gripCloseAction = playerInput.actions.FindAction("GripClose");
 
-        m_pokerOutAction = m_playerInput.actions.FindAction("PokerOut");
-        m_pokerInAction = m_playerInput.actions.FindAction("PokerIn");
+        pokerOutAction = playerInput.actions.FindAction("PokerOut");
+        pokerInAction = playerInput.actions.FindAction("PokerIn");
         
         // Assign event functions ------------------------------------------------------------
 
-        /*m_aimLockAction.started += OnAimLock;
-        m_aimLockAction.canceled += OnAimLock;*/
+        /*aimLockAction.started += OnAimLock;
+        aimLockAction.canceled += OnAimLock;*/
     }
 
     public static InputManager GetInstance()
--- Arm_IK_Sim_Unity/Assets/Scripts/IK_Controller.cs
+++ Arm_IK_Sim_Unity/Assets/Scripts/IK_Controller.cs
@@ -6,16 +6,16 @@ using UnityEngine;
 public class IK_Controller : MonoBehaviour
 {
     [Header("Speed Adjustment")]
-    [SerializeField] private float m_turretRotationSpeed = 25;
-    [SerializeField] private float m_armMovementSpeed = 5;
-    [SerializeField] private float m_armMovementOtherSpeed = 3;
+    [SerializeField] private float turretRotationSpeed = 25;
+    [SerializeField] private float armMovementSpeed = 5;
+    [SerializeField] private float armMovementOtherSpeed = 3;
 
     [Header("Limits")]
-    [SerializeField] private bool m_IKTargetLimits = true;
-    [SerializeField] private bool m_TurretRotLimits = true;
+    [SerializeField] private bool IKTargetLimits = true;
+    [SerializeField] private bool turretRotLimits = true;
 
     [SerializeField]
-    private float[,] m_IKLimits = {
+    private float[,] IKLimits = {
 
         {-13.8f, -7f},   // IK Target  = (Min X, Max X)   
         {-4.67f, 10.8f}, // IK Target  = (Min Y, Max Y)
@@ -24,8 +24,8 @@ public class IK_Controller : MonoBehaviour
     };
 
     [Header("IKJoints")]
-    [SerializeField] private Transform m_turretJoint;
-    [SerializeField] private Transform m_IKTarget;
+    [SerializeField] private Transform turretJoint;
+    [SerializeField] private Transform IKTarget;
 
     private InputManager inputManager;
 
@@ -37,54 +37,54 @@ public class IK_Controller : MonoBehaviour
 
     private void Update()
     {
-        Vector2 movement = inputManager.m_armPlanarMovementAction.ReadValue<Vector2>();
+        Vector2 movement = inputManager.armPlanarMovementAction.ReadValue<Vector2>();
 
         // Move IK target up and down
-        m_IKTarget.localPosition += new Vector3(0, movement.y * m_armMovementSpeed * Time.deltaTime);
+        IKTarget.localPosition += new Vector3(0, movement.y * armMovementSpeed * Time.deltaTime);
 
         // Rotate turret joint
-        m_turretJoint.localRotation = Quaternion.Euler(m_turretJoint.localEulerAngles.x, m_turretJoint.localEulerAngles.y + (m_turretRotationSpeed * Time.deltaTime * movement.x), m_turretJoint.localEulerAngles.z);
+        turretJoint.localRotation = Quaternion.Euler(turretJoint.localEulerAngles.x, turretJoint.localEulerAngles.y + (turretRotationSpeed * Time.deltaTime * movement.x), turretJoint.localEulerAngles.z);
 
         // Move IK target forward or backward.
-        if (inputManager.m_armForwardAction.IsPressed())
+        if (inputManager.armForwardAction.IsPressed())
         {
-            m_IKTarget.localPosition += new Vector3(-m_armMovementOtherSpeed * Time.deltaTime, 0);
+            IKTarget.localPosition += new Vector3(-armMovementOtherSpeed * Time.deltaTime, 0);
         }
-        else if (inputManager.m_armBackwardAction.IsPressed())
+        else if (inputManager.armBackwardAction.IsPressed())
         {
-            m_IKTarget.localPosition += new Vector3(m_armMovementOtherSpeed * Time.deltaTime, 0);
+            IKTarget.localPosition += new Vector3(armMovementOtherSpeed * Time.deltaTime, 0);
         }
 
         // Enforce IK target bounds -------------------------------
 
-        if (m_IKTargetLimits)
+        if (IKTargetLimits)
         {
-            Vector3 localPos = m_IKTarget.localPosition;
+            Vector3 localPos = IKTarget.localPosition;
 
-            if (localPos.x > m_IKLimits[0, 1])
+            if (localPos.x > IKLimits[0, 1])
             {
-                localPos.x = m_IKLimits[0, 1];
+                localPos.x = IKLimits[0, 1];
             }
-            else if (localPos.x < m_IKLimits[0, 0])
+            else if (localPos.x < IKLimits[0, 0])
             {
-                localPos.x = m_IKLimits[0, 0];
-            }
-
-            if (localPos.y > m_IKLimits[1, 1])
-            {
-                localPos.y = m_IKLimits[1, 1];
-            }
-            else if (localPos.y < m_IKLimits[1, 0])
-            {
-                localPos.y = m_IKLimits[1, 0];
+                localPos.x = IKLimits[0, 0];
             }
 
-            m_IKTarget.localPosition = localPos;
+            if (localPos.y > IKLimits[1, 1])
+            {
+                localPos.y = IKLimits[1, 1];
+            }
+            else if (localPos.y < IKLimits[1, 0])
+            {
+                localPos.y = IKLimits[1, 0];
+            }
+
+            IKTarget.localPosition = localPos;
         }
 
         // Enforce turret rotation limits ------------------------------
 
-        if (m_TurretRotLimits)
+        if (turretRotLimits)
         {
             // TODO
         }
