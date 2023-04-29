fea05345ad3021c1b4160b19c2913008f7a768ec
Cameron Wolff <36940948+camwolff02@users.noreply.github.com>
2023-04-28 21:01:52 -0700

    added support for linear actuator commands

--- py_pubsub/py_pubsub/arm_controller.py
+++ py_pubsub/py_pubsub/arm_controller.py
@@ -30,6 +30,8 @@ TURRET_ACCEL = 700
 # l (inches) - horizontal length between linear actuator base and arm hinge
 actuator_tri = namedtuple('actuator_tri', 'a b theta')
 
+
+# dataclass wrappers for writing arm data to motors
 @dataclass(frozen=True)
 class Angle:
     angle_min : float
@@ -41,45 +43,44 @@ class Encoder:
     encoder_min : int
 
 @dataclass(frozen=True)
-class Linear_Actuator(Encoder, Angle):
+class LinearActuator(Encoder, Angle):
     length_min : float 
     length_max : float 
     position_on_arm : actuator_tri 
 
 @dataclass(frozen=True)
-class Rotation_Motor(Encoder, Angle):
+class RotationMotor(Encoder, Angle):
     ...
 
-@dataclass(frozen=True)
-class Gripper_Motor():
-    ...
 
-class Motor_Controller:
+class MotorController:
+    """Wrapper class for Roboclaw Motor Controller interfacing"""
     def __init__(self, rc: Roboclaw, address: int, m1: Encoder, m2: Encoder):
         self.rc, self.address = rc, address
         self.m1, self.m2 = m1, m2
 
         self.rc.Open()
 
-    # Print speed and encoder values of both motors to terminal
     def print_encoders(self) -> None:
+        """Print speed and encoder values of both motors to terminal"""
         enc1 = self.rc.ReadEncM1(self.address)
         print(f"M1: {enc1[1]}" if enc1[0] == 1 else "disconnected", end=' ')
         enc2 = self.rc.ReadEncM2(self.address)
         print(f"M2: {enc2[1]}" if enc2[0] == 1 else "disconnected")
 
 
+def angle_to_enc(motor : LinearActuator or RotationMotor, angle : float) -> int:
+    """Convert angle to encoder value taking into account min/max encoder values and angle values.
+    uses linear approximation of true encoder values, real implementations should be exponential"""
 
-#NOTE correctly maps angle and encoder values at limits, but loses accuracy the closer it gets to center
-# Convert angle to encoder value taking into account min/max encoder values and angle values.
-def angle_to_enc(motor : Linear_Actuator or Rotation_Motor, angle : float) -> int:
     slope = (motor.encoder_max - motor.encoder_min) / (motor.angle_max - motor.angle_min)
     
     return int(slope * (angle - motor.angle_min) + motor.encoder_min)
 
-#NOTE use encoder.angle_to_enc for now, angle_to_length not currently working
-# Convert angle linear actuator lengths taking into account min/max angles and length values.
-def angle_to_length(motor : Linear_Actuator, angle : float) -> float:
+
+def angle_to_length(motor : LinearActuator, angle : float) -> float:
+    """NOTE use encoder.angle_to_enc for now, angle_to_length not currently working
+    Convert angle linear actuator lengths taking into account min/max angles and length values."""
     a, b, theta = motor.position_on_arm 
     
     # Law of cosines
@@ -87,29 +88,26 @@ def angle_to_length(motor : Linear_Actuator, angle : float) -> float:
 
     return actuator_length
 
-#NOTE correctly maps between length and encoder values
-# Convert linear actuator lengths to encoder value taking into account min/max lengths and angle values.
-def length_to_enc(motor : Linear_Actuator, length : float) -> int:
+
+def length_to_enc(motor : LinearActuator, length : float) -> int:
+    """# Convert linear actuator lengths to encoder value 
+    taking into account min/max lengths and angle values."""
     slope = (motor.encoder_max - motor.encoder_min) / (motor.length_max - motor.length_min)
     
     return int(slope * (length - motor.length_min) + motor.encoder_min)
 
 
+def scale_actuator_length(motor: LinearActuator, length: float) -> int:
+    """ linear interpolation of normalized actuator length to real encoder value
+    y = mx + b: m = (encoder_max-encoder_min), b = encoder_min"""
+    return length * (motor.encoder_max - motor.encoder_min) + motor.encoder_min
 
-# Set position of arm using PID function.
-def set_arm_position(mcp: Motor_Controller, bicep_angle: float, forearm_angle: float) -> None:
 
-    encoder_val_m1 = angle_to_enc(mcp.m1, bicep_angle)
-    #encoder_val_m1 = length_to_enc(mcp.m1, angle_to_length(mcp.m1, bicep_angle))
-    encoder_val_m2 = angle_to_enc(mcp.m2, forearm_angle)
-    
+def set_arm_position(mcp: MotorController, bicep_actuator_len: float, forearm_actuator_len: float) -> None:
+    """Set position of arm using PID function."""
+    encoder_val_m1 = scale_actuator_length(mcp.m1, bicep_actuator_len)
+    encoder_val_m2 = scale_actuator_length(mcp.m2, forearm_actuator_len)
 
-    # encoder_val_m1 = length_to_enc(mcp.m1, angle_to_length(mcp.m1, bicep_angle))
-    # encoder_val_m2 = length_to_enc(mcp.m2, angle_to_length(mcp.m2, forearm_angle))
-
-    # print(bicep_angle, " ", encoder_val_m1, " ", )
-
-    # print(encoder_val_m1, " ", bicep_angle, " ", encoder_val_m2, " ", forearm_angle)    
 
     mcp.rc.SpeedAccelDeccelPositionM1M2(mcp.address, 
         ACCELERATION, SPEED, ACCELERATION, encoder_val_m1, 
@@ -117,21 +115,20 @@ def set_arm_position(mcp: Motor_Controller, bicep_angle: float, forearm_angle: f
         BUFFER_OR_INSTANT 
     )
 
-def set_arm_rotation(mcp: Motor_Controller, base_angle : float) -> None:
+
+def set_arm_rotation(mcp: MotorController, base_angle : float) -> None:
+    """Set rotation of arm using PID function."""
     encoder_val = angle_to_enc(mcp.m2, base_angle)
-    #encoder_val = length_to_enc(mcp.m1, mcp.m1.angle_to_length(mcp.m1, base_angle))
 
     mcp.rc.SpeedAccelDeccelPositionM2(mcp.address, 
         TURRET_ACCEL, TURRET_SPEED, TURRET_ACCEL, encoder_val, BUFFER_OR_INSTANT
     )
 
 
-def set_hand_rotation(mcp: Motor_Controller, hand_pitch: float, hand_roll: float) -> None:
-
+def set_hand_rotation(mcp: MotorController, hand_pitch: float, hand_roll: float) -> None:
+    """Set rotation of hand using PID function."""
     encoder_val_m1 = angle_to_enc(mcp.m1, hand_roll)
     encoder_val_m2 = angle_to_enc(mcp.m2, hand_pitch)
-    #encoder_val_m1 = length_to_enc(mcp.m1, angle_to_length(mcp.m1, hand_pitch))
-    #encoder_val_m2 = length_to_enc(mcp.m2, angle_to_length(mcp.m2, hand_roll))
 
     mcp.rc.SpeedAccelDeccelPositionM1M2(mcp.address, 
         ROLL_ACCEL, ROLL_SPEED, ROLL_ACCEL, encoder_val_m1, 
@@ -140,13 +137,9 @@ def set_hand_rotation(mcp: Motor_Controller, hand_pitch: float, hand_roll: float
     )
 
 
-def open_close_hand(mcp: Motor_Controller, move_velocity):
-    # encoder_val = mcp.rc.ReadEncM1(mcp.address)
-
+def open_close_hand(mcp: MotorController, move_velocity):
+    """Set velocity of end effector grippers"""
     if move_velocity == 0:
         mcp.rc.ForwardM1(mcp.address, 0)
     else:
-        # mcp.rc.SpeedAccelM1(mcp.address, 20, int(move_velocity))
         mcp.rc.SpeedAccelM1(mcp.address, 20, int(move_velocity))
-
-# rm -rf build install log && colcon build && source ./install/setup.bash && ros2 run py_pubsub listener
---
+++ py_pubsub/py_pubsub/arm_driver.py
@@ -0,0 +1,245 @@
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Ros2 imports
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32MultiArray
+
+# General imports
+from time import perf_counter
+from .arm_controller import *
+import signal
+import subprocess
+import os
+
+
+class ArmDriver(Node):
+
+    # Quit program safely
+    def quit_program_safely(self):
+
+        print("\nExited Safely")
+        self.SoftStop()
+        quit()
+
+
+    # Callback for Ctrl+C
+    def signalHandler(self, signal, frame):
+        self.quit_program_safely()
+
+
+    def __init__(self):
+        # Signal handler for Ctrl+C
+        signal.signal(signal.SIGINT, self.signalHandler)
+
+        # Get motor controller device paths
+        self.mcp_comport_list = self.get_motor_controllers()
+
+        # TODO - need to have these motors use PID position rather than encoder values? DO for mcp3 as well
+        # May want to do this for lin actuators as well. need to test 
+        
+        # Initialize motor controllers
+        # try:
+        self.initialize_motor_controllers()
+        
+
+        # TODO - Eventually save previously known value for quad encoders. Do we need this for now?
+        # Set encoder values to zero
+        # self.mcp1.rc.SetEncM1(self.mcp1.address, 0) # Roll
+        # self.mcp1.rc.SetEncM2(self.mcp1.address, 0) # Pitch
+        # self.mcp3.rc.SetEncM2(self.mcp3.address, 0) # Turret
+
+        # Give the node a name.
+        super().__init__('arm_driver')
+
+        # Subscribe to the topic 'topic'. Callback gets called when a message is received.
+        self.subscription = self.create_subscription(
+            Float32MultiArray,
+            '/arm/control_instruction',
+            self.subscriber_callback,  # listener callback
+            10)
+        self.subscription  # prevent unused variable warning
+
+
+        self.time_of_last_callback = perf_counter()
+        self.timer_period = 0.1 # Interval for checking heartbeat.
+        self.signal_timeout = 3 # Max time before soft stop if subscription heartbeat is not detected.
+        self.timer = self.create_timer(self.timer_period, self.subscription_heartbeat)
+
+
+    # For safety. Check if controls are being received by subscriber callback
+    # Stop motors if subscription not being received.
+    def subscription_heartbeat(self):
+        if perf_counter() - self.time_of_last_callback > self.signal_timeout:
+            self.SoftStop()
+
+
+    # Signal to stop all motors
+    def SoftStop(self):
+        
+        print("Soft Stop Triggered")
+
+        for mcp in self.MCP_List:
+            if mcp is not None:
+                mcp.rc.ForwardM1(mcp.address, 0)
+                mcp.rc.ForwardM2(mcp.address, 0)
+
+
+    # called every time the subscriber receives a message
+
+    # Arm control instruction:
+    # [bicep_actuator_len, forearm_actuator_len, bicep_angle, forearm_angle, base_angle, pitch_angle, roll_angle, finger_velocity, poker_velocity]
+    def subscriber_callback(self, msg):
+
+        self.time_of_last_callback = perf_counter()
+
+        bicep_actuator_len, forearm_actuator_len = msg.data[0], msg.data[1], 
+        base_angle = msg.data[4]
+        pitch_angle, roll_angle, finger_velocity = msg.data[5], msg.data[6], msg.data[7]
+        
+        # only change the arm position if any angles have changed
+        set_arm_position(self.mcp2, bicep_actuator_len, forearm_actuator_len)
+
+        set_arm_rotation(self.mcp3, base_angle)
+
+        # Hand pitch and roll
+        set_hand_rotation(self.mcp1, pitch_angle, roll_angle)
+
+        # Finger movement
+        open_close_hand(self.mcp3, finger_velocity)
+
+        # This prints an info message to the console, along with the data it received. 
+        # for x in msg.data: print(x, end=' ')
+        # print()
+
+
+    # Get motor controller device paths using serial IDs
+    # Returns in order (0001, 0002, 0003)
+    def get_motor_controllers(self):
+
+        # Bash script is not moving during colcon build, and so am just pasting it here.
+        # TODO - Fix this
+
+        getter_script = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../../../share/py_pubsub/find_devpath.bash')
+
+        device_list = subprocess.run([getter_script], stdout=subprocess.PIPE, text=True, shell=True).stdout.splitlines()
+
+        devpath_list = ["", "", ""]
+
+        # Add device paths to devpath list
+        for device in device_list:
+            splitStr = device.split(" - ")
+
+            if "ID0001" in splitStr[1]:
+                devpath_list[0] = splitStr[0]
+
+            elif "ID0002" in splitStr[1]:
+                devpath_list[1] = splitStr[0]
+
+            elif "ID0003" in splitStr[1]:
+                devpath_list[2] = splitStr[0]
+
+        return devpath_list
+        
+
+    # Initialize motor controller objects
+    def initialize_motor_controllers(self):
+            
+            self.MCP_List = [None, None, None]
+
+            # motor controller for end-effector pitch and roll
+            self.mcp1 = MotorController(
+                rc = Roboclaw(self.mcp_comport_list[0], 115200),
+                address = 0x80,  
+                m1 = Rotation_Motor(  # roll 
+                    encoder_max = 6580,      # Max PID position
+                    encoder_min = -6583,        # Min PID position
+                    angle_max   = 90,        # retract
+                    angle_min   = -90,         # extend
+                ),
+                m2 = Rotation_Motor(  # pitch
+                    encoder_max = 7656,         # Max PID position
+                    encoder_min = -6722,           # Min PID position
+                    angle_max   = 90,          # retract
+                    angle_min   = -90,           # extend
+                )
+            )
+
+
+            # motor controller for forearm and bicep linear actuators
+            self.mcp2 = MotorController(
+                rc = Roboclaw(self.mcp_comport_list[1], 115200),
+                address = 0x80,  
+                m1 = Linear_Actuator(  # bicep 
+                    encoder_max = 2633,      # retract
+                    encoder_min = 153,        # extend
+                    angle_max   = 75,        # retract
+                    angle_min   = 5,         # extend
+                    length_max  = 34.2138,   # extend, centimeters
+                    length_min  = 24.2062,   # retract, centimeters
+                    # position_on_arm = actuator_pos(7.16717277, 1.0, 6.5)  # inches
+                    position_on_arm = actuator_tri(16.7042, 18.2046, 8.75) # (cm, cm, deg)
+                ),
+                m2 = Linear_Actuator(  # forearm
+                    encoder_max = 1893,         # retract
+                    encoder_min = 20,           # extend
+                    angle_max   = 140,          # retract
+                    angle_min   = 75,           # extend
+                    length_max  = 34.2138,      # extend, centimeters
+                    length_min  = 25.4762,      # retract, centimeters
+                    # position_on_arm = actuator_tri(3.0, 1.125, 12.50719421)  # inches
+                    position_on_arm = actuator_tri(31.8965, 7.62, 5.14) # (cm, cm, deg)
+                )
+            )
+
+
+            # Motor controller for turret and grip
+            self.mcp3 = MotorController(
+                rc = Roboclaw(self.mcp_comport_list[2], 115200),
+                address = 0x80,  
+                m1 = Gripper_Motor(  # Grip 
+                    # Note: Grip motor does not have encoder
+                ),
+                m2 = Rotation_Motor(  # Turret
+                    # Note: No limit needed for turret. Rotate via velocity rather than position.
+                    angle_min = -120,
+                    angle_max = 120,
+                    encoder_max = 13993,
+                    encoder_min = -11314
+                )
+            )
+
+            # Create list of motor controllers
+            self.MCP_List = [self.mcp1, self.mcp2, self.mcp3]
+
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    arm_driver = ArmDriver()
+
+    rclpy.spin(arm_driver)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    arm_driver.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
